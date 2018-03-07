7b7a4d3d3f80baba08f03de5f748242db0abf0b8
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-07 20:29:10 +0100

    Create index.js

---
+++ nya-bot/index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link itemprop="image" content="fluid-icon" href="https://cdn.discordapp.com/avatars/377888169355640832/82722c0864c0c02c5e1352016c38db2e.png?size=2048">
+        <title>Nya!bot</title>
+    </head>
+    <body>
+    Test
+    </body>
+</html>
