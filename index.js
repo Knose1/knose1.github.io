dc3ac6821016157fead380054e0d092442394a0d
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-08 13:14:49 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-var scripting = document.createElement("SCRIPT")
-    .type = "text/javascript"
-    .src = "/data/repositories.json";
+var scripting = document.createElement("SCRIPT");
+scripting.type = "text/javascript";
+scripting.src = "repositories.json";
 
 document.head.appendChild(scripting);
+
+repositories.forEach( m => {
+
+    var divv = document.createElement("div");
+
+});
