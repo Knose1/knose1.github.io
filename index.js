32459284575c8f392a4f6dacd8e24b38f725d333
Knose1 <34002600+Knose1@users.noreply.github.com>
2018-03-09 11:22:22 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-async function repoLoad() {
-    function repoaw() {
-        var scripting = document.createElement("SCRIPT");
-        scripting.type = "text/javascript";
-        scripting.src = "repositories.json";
+repositories.forEach( m => {
     
-        document.body.appendChild(scripting);
-        return new Promise(resolve => {
-            setTimeout(() => {
-            }, 2000);
-        });
-    }
-    await repoaw();
-    
-    repositories.forEach( m => {
-    
-        var divv = document.createElement("DIV");
-        divv.id = "rep";
-        var tags = "";
-        m.tags.forEach(tag => {
-            tags += `<span class="tag">${tag}</span>`
-        });
-        
-        divv.innerHTML =    `<a href="./${m.url}"> ${m.name} </a>` +
-                            `<div class="typecode">
-                                <span class="circle", id="${m.code.toLowerCase()}"></span> 
-                                <span class="codename"> ${m.code} </span>
-                             </div>` +
-                            tags;
-        document.body.appendChild(divv);
+    var divv = document.createElement("DIV");
+    divv.id = "rep";
+    var tags = "";
+    m.tags.forEach(tag => {
+        tags += `<span class="tag">${tag}</span>`
     });
-}
-
-repoLoad()
+        
+    divv.innerHTML =    `<a href="./${m.url}"> ${m.name} </a>` +
+                        `<div class="typecode">` +
+                            `<span class="circle", id="${m.code.toLowerCase()}"></span>` +
+                            `<span class="codename"> ${m.code} </span>` +
+                        `</div>` +
+                        tags;
+    document.body.appendChild(divv);
+});
