d237c748bcbe2b7bdd08a5f202c74d02df9d826f
Knose1 <34002600+Knose1@users.noreply.github.com>
2020-09-14 02:29:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-(() => {
-    let lDivv;
-    let lBr;
-    let lTags;
-    repositories.forEach( pData => {
+fetch("https://api.github.com/users/Knose1/repos").then((r) => {
+    r.json().then(repositories => () => {
+    
+        let lDivv;
+        let lBr;
+        let lTags;
+        repositories.forEach( pData => {
 
-        lDivv = document.createElement("DIV");
-        lBr = document.createElement("BR");
-        lDivv.id = "rep";
-        lTags = "";
-        pData.tags.forEach(tag => {
-            lTags += `<span class="tag">${tag}</span>`
+            lDivv = document.createElement("DIV");
+            lBr = document.createElement("BR");
+            lDivv.id = "rep";
+            lTags = "";
+            pData.tags.forEach(tag => {
+                lTags += `<span class="tag">${tag}</span>`
+            });
+
+            lDivv.innerHTML =    `<a href="./${pData.url}"> ${pData.name} </a>` +
+                                `<div class="typecode">` +
+                                    `<span class="circle", id="${pData.code.toLowerCase()}"></span>` +
+                                    `<span class="codename"> ${pData.code} </span>` +
+                                `</div>` +
+                                lTags;
+            document.body.appendChild(lDivv);
+            document.body.appendChild(lBr);
         });
-
-        lDivv.innerHTML =    `<a href="./${pData.url}"> ${pData.name} </a>` +
-                            `<div class="typecode">` +
-                                `<span class="circle", id="${pData.code.toLowerCase()}"></span>` +
-                                `<span class="codename"> ${pData.code} </span>` +
-                            `</div>` +
-                            lTags;
-        document.body.appendChild(lDivv);
-        document.body.appendChild(lBr);
+    
     });
-})()
+});
